d7d974174e86bfdae26d1b5b44a7c68b8f9addeb
Carlos <carlosaac232001@gmail.com>
2025-09-19 11:59:35 -0500

    feat: create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className='mb-4'>
+      <p className='text-center text-sm text-neutral-500'>
+        APV - Derechos Reservados {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
