148f971309986dd17adcf20fa7893683002a5814
Carlos <carlosaac232001@gmail.com>
2025-09-20 22:34:45 -0500

    feat: import and use all new components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { AuthProvider } from '@/context/AuthProvider';
 import AuthLayout from '@/layout/AuthLayout';
+import AdminLayout from '@/layout/AdminLayout';
+
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import ForgotPassword from '@/pages/ForgotPassword';
 import VerifyAccount from '@/pages/VerifyAccount';
+import NewPassword from '@/pages/NewPassword';
+import Admin from '@/pages/Admin';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='forgot-password' element={<ForgotPassword />} />
-          <Route path='verify/:token' element={<VerifyAccount />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='forgot-password' element={<ForgotPassword />} />
+            <Route path='forgot-password/:token' element={<NewPassword />} />
+            <Route path='verify/:token' element={<VerifyAccount />} />
+          </Route>
+
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<Admin />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
