17eeaf9130cb7612467d2bd8461b881130f5239f
Carlos <carlosaac232001@gmail.com>
2025-09-22 00:34:15 -0500

    feat: create ChangePassword page to admin

---
+++ src/pages/ChangePassword.jsx
@@ -0,0 +1,13 @@
+import AdminNav from '@/components/AdminNav';
+
+export default function ChangePassword() {
+  return (
+    <>
+      <AdminNav />
+      <h2 className='mt-4 text-center text-xl font-bold text-sky-500'>
+        Cambiar Contraseña
+      </h2>
+      <p className='text-center'>Modifica tu contraseña aquí</p>
+    </>
+  );
+}
