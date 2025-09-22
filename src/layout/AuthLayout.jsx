bcbeb60509fbea6d65c2a0e3430f039c76d394c9
Carlos <carlosaac232001@gmail.com>
2025-09-21 22:08:44 -0500

    style: wrap Footer component into container to fixed to botto

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router';
+import Footer from '@/components/Footer';
 
 export default function AuthLayout() {
   return (
     <main className='mx-4 mt-4 md:grid md:grid-cols-2'>
       <Outlet />
+      <div className='fixed right-0 bottom-0 left-0'>
+        <Footer />
+      </div>
     </main>
   );
 }
