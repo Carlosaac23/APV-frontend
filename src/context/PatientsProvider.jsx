b8bc922f39349b4869bb59cb63930bcd86498467
Carlos <carlosaac232001@gmail.com>
2025-09-22 00:33:01 -0500

    feat: add delete and edit function with states management

--- src/context/PatientsProvider.jsx
+++ src/context/PatientsProvider.jsx
@@ -6,6 +6,7 @@ const PatientsContext = createContext();
 
 export function PatientsProvider({ children }) {
   const [patients, setPatients] = useState([]);
+  const [patient, setPatient] = useState({});
 
   useEffect(() => {
     async function getPatients() {
@@ -31,25 +32,73 @@ export function PatientsProvider({ children }) {
   }, []);
 
   async function savePatient(patient) {
-    try {
-      const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await axiosClient.post('/patients', patient, config);
-      const { createdAt, updatedAt, __v, ...storedPatient } = data;
-      setPatients([storedPatient, ...patients]);
-    } catch (error) {
-      toast.error(error.response.data.msg);
+    if (patient.id) {
+      try {
+        const { data } = await axiosClient.put(
+          `/patients/${patient.id}`,
+          patient,
+          config,
+        );
+
+        const updatedPatients = patients.map((statePatient) =>
+          statePatient._id === data._id ? data : statePatient,
+        );
+        setPatients(updatedPatients);
+        toast.success('Holaaaaa');
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const { data } = await axiosClient.post('/patients', patient, config);
+        const { createdAt, updatedAt, __v, ...storedPatient } = data;
+        setPatients([storedPatient, ...patients]);
+      } catch (error) {
+        toast.error(error.response.data.msg);
+      }
+    }
+  }
+
+  function setEdition(patient) {
+    setPatient(patient);
+  }
+
+  async function deletePatient(id) {
+    const confirmMsg = confirm('¿Seguro que quieres eliminar este paciente?');
+    if (confirmMsg) {
+      try {
+        const token = localStorage.getItem('token');
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const { data } = await axiosClient.delete(`/patients/${id}`, config);
+        const updatedPatients = patients.filter(
+          (statePatients) => statePatients._id !== id,
+        );
+        setPatients(updatedPatients);
+        toast.info('Paciente eliminado');
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 
   return (
-    <PatientsContext.Provider value={{ patients, savePatient }}>
+    <PatientsContext.Provider
+      value={{ patients, savePatient, setEdition, patient, deletePatient }}
+    >
       {children}
     </PatientsContext.Provider>
   );
