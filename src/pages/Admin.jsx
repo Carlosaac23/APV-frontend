5544334de243e078c3907933f0aa91fdcfb0cc25
Carlos <carlosaac232001@gmail.com>
2025-09-21 22:09:14 -0500

    feat: create Admin index page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,3 +1,43 @@
+import { useState } from 'react';
+import Form from '@/components/Form';
+import PatientsList from '@/components/PatientsList';
+import Footer from '@/components/Footer';
+
 export default function Admin() {
-  return <h1>Admin</h1>;
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <>
+      <div className='flex flex-col md:flex-row'>
+        <button
+          type='button'
+          className='mx-auto rounded-md bg-sky-500 px-6 py-2 text-neutral-50 shadow-sm transition-transform duration-150 ease-out hover:cursor-pointer hover:bg-sky-600 active:scale-97 md:hidden'
+          onClick={() => setShowForm(!showForm)}
+        >
+          {showForm ? 'Ocultar Formulario' : 'Mostrar Formulario'}
+        </button>
+        <div
+          className={` ${showForm ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}
+        >
+          <Form />
+        </div>
+
+        <div className='md:w-1/2 lg:w-3/5'>
+          <PatientsList />
+        </div>
+      </div>
+
+      <div>
+        {showForm ? (
+          <Footer />
+        ) : (
+          <>
+            <div className='fixed right-0 bottom-0 left-0'>
+              <Footer />
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
