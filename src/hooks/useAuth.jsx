7b8057193d8c6c4255e5bd8874cd2792ecca5cf9
Carlos <carlosaac232001@gmail.com>
2026-01-22 22:40:07 -0500

    chore: normal export instead of default export

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import AuthContext from '@/context/AuthProvider';
 
-export default function useAuth() {
+export function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth debe ser usado dentro de un AuthProvider');
