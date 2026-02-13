88885fae5f50f91bea3f060e326bd867dfea9b75
Carlos <carlosaac232001@gmail.com>
2026-02-13 09:02:04 -0500

    chore: add Footer root so it will be available in all views

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/App.jsx';
+import Footer from '@/components/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='flex min-h-screen flex-col'>
+      <App />
+      <Footer />
+    </div>
   </StrictMode>
 );
