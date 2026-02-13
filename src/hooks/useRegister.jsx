5703f2a549ffec664beec215ebebdc736174af23
Carlos <carlosaac232001@gmail.com>
2026-02-13 09:00:45 -0500

    feat: create first custom hook

---
+++ src/hooks/useRegister.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export function useRegister() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = e => {
+    setConfirmPassword(e.target.value);
+  };
+
+  return {
+    name,
+    handleNameChange,
+    email,
+    handleEmailChange,
+    password,
+    handlePasswordChange,
+    confirmPassword,
+    handleConfirmPasswordChange,
+  };
+}
