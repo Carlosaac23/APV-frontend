17f6f59fd6e8a0fcec4edff1895760f2cfa454b0
Carlos <carlosaac232001@gmail.com>
2026-02-13 10:05:59 -0500

    feat: add Sonner toasts to project

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import '@/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner';
 
 import App from '@/App.jsx';
 import Footer from '@/components/Footer';
@@ -9,6 +10,7 @@ import Footer from '@/components/Footer';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='flex min-h-screen flex-col'>
+      <Toaster richColors={true} />
       <App />
       <Footer />
     </div>
