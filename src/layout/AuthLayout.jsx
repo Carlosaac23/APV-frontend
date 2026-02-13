1cd60403dae9e02118cbc888a1cf1b0aaeff87be
Carlos <carlosaac232001@gmail.com>
2026-02-13 09:01:03 -0500

    feat: create main layout

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+export default function AuthLayout() {
+  return (
+    <>
+      <main className='container mx-auto mt-12 items-center gap-24 p-5 md:grid md:grid-cols-2'>
+        <Outlet />
+      </main>
+    </>
+  );
+}
