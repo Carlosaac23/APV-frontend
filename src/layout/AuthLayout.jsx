60c2a9f30df375bc0a03cec894dbba9ca2e52f21
Carlos <carlosaac232001@gmail.com>
2026-03-05 17:44:14 -0500

    chore: remove unnecesary fragment

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -2,10 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export default function AuthLayout() {
   return (
-    <>
-      <main className='container mx-auto mt-12 items-center gap-24 p-5 md:grid md:grid-cols-2'>
-        <Outlet />
-      </main>
-    </>
+    <main className='container mx-auto mt-12 items-center gap-24 p-5 md:grid md:grid-cols-2'>
+      <Outlet />
+    </main>
   );
 }
