59e2cd56b5f5a812394f8a9e8664495471459ba7
Carlos <carlosaac232001@gmail.com>
2026-03-05 18:07:10 -0500

    feat: add Header component to the top of the page and use BrowserRouter to wrap all app here

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import '@/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
 import App from '@/App.jsx';
 import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='flex min-h-screen flex-col'>
-      <Toaster richColors={true} />
-      <App />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className='flex min-h-screen flex-col'>
+        <Toaster richColors={true} />
+        <Header />
+        <App />
+        <Footer />
+      </div>
+    </BrowserRouter>
   </StrictMode>
 );
