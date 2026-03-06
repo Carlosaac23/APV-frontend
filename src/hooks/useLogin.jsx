b131629b0f537b895468ec41e5ce6ac8e73e76d5
Carlos <carlosaac232001@gmail.com>
2026-03-05 19:14:35 -0500

    feat: create custom hook to manage all login logic

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+
+import { axiosClient } from '@/config/axios';
+import { useAuth } from '@/hooks/useAuth';
+
+export function useLogin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { authUser, setLoading } = useAuth();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email === '') {
+      toast.error('Email is required');
+      return;
+    }
+    if (password === '') {
+      toast.error('Password is required');
+      return;
+    }
+
+    try {
+      const { data } = await axiosClient.post('/veterinarians/login', {
+        email,
+        password
+      });
+      localStorage.setItem('apv_token', data.token);
+      setLoading(true);
+      await authUser();
+      navigate('/admin');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      setLoading(false);
+    }
+  }
+
+  return { email, setEmail, password, setPassword, handleSubmit };
+}
