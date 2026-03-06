7e3198154a4abe4f15cc9c68021bab263e42d91d
Carlos <carlosaac232001@gmail.com>
2026-03-05 19:08:50 -0500

    feat: import new pages and move BrowserRouter to main.jsx. create protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
+import { AuthProvider } from '@/context/AuthProvider';
+import AdminLayout from '@/layout/AdminLayout';
 import AuthLayout from '@/layout/AuthLayout';
+import About from '@/pages/About';
+import AdminPatient from '@/pages/AdminPatient';
 import ConfirmAccount from '@/pages/ConfirmAccount';
 import ForgotPassword from '@/pages/ForgotPassword';
+import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import ResetPassword from '@/pages/ResetPassword';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
       <Routes>
         <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
           <Route path='confirm/:token' element={<ConfirmAccount />} />
           <Route path='forgot-password' element={<ForgotPassword />} />
           <Route path='forgot-password/:token' element={<ResetPassword />} />
           <Route path='register' element={<Register />} />
+          <Route path='about' element={<About />} />
+        </Route>
+
+        {/* Protected Routes */}
+        <Route path='/admin' element={<AdminLayout />}>
+          <Route index element={<AdminPatient />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </AuthProvider>
   );
 }
