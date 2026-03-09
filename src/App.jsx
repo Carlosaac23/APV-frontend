520dd779b2ff002a5a4fee835bb16acfefc92431
Carlos <carlosaac232001@gmail.com>
2026-03-08 22:19:35 -0500

    refactor: remove AuthProvider wrapper and clean up routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { AuthProvider } from '@/context/AuthProvider';
 import AdminLayout from '@/layout/AdminLayout';
 import AuthLayout from '@/layout/AuthLayout';
 import About from '@/pages/About';
-import AdminPatient from '@/pages/AdminPatient';
+import AdminPatients from '@/pages/AdminPatients';
 import ConfirmAccount from '@/pages/ConfirmAccount';
 import ForgotPassword from '@/pages/ForgotPassword';
 import Home from '@/pages/Home';
@@ -14,23 +13,21 @@ import ResetPassword from '@/pages/ResetPassword';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='confirm/:token' element={<ConfirmAccount />} />
-          <Route path='forgot-password' element={<ForgotPassword />} />
-          <Route path='forgot-password/:token' element={<ResetPassword />} />
-          <Route path='register' element={<Register />} />
-          <Route path='about' element={<About />} />
-        </Route>
+    <Routes>
+      <Route path='/' element={<AuthLayout />}>
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='confirm/:token' element={<ConfirmAccount />} />
+        <Route path='forgot-password' element={<ForgotPassword />} />
+        <Route path='forgot-password/:token' element={<ResetPassword />} />
+        <Route path='register' element={<Register />} />
+        <Route path='about' element={<About />} />
+      </Route>
 
-        {/* Protected Routes */}
-        <Route path='/admin' element={<AdminLayout />}>
-          <Route index element={<AdminPatient />} />
-        </Route>
-      </Routes>
-    </AuthProvider>
+      {/* Protected Routes */}
+      <Route path='/admin' element={<AdminLayout />}>
+        <Route index element={<AdminPatients />} />
+      </Route>
+    </Routes>
   );
 }
