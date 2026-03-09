5a8969874a24b52bfd33574fed0aecaf5e4ef60a
Carlos <carlosaac232001@gmail.com>
2026-03-08 22:22:26 -0500

    feat: update Header component to conditionally render navigation links based on authentication status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import { useAuth } from '@/context/AuthProvider';
+
 export default function Header() {
+  const { auth, logout } = useAuth();
+  console.log(auth);
+
   return (
     <header className='flex items-center justify-between bg-sky-500 px-10 py-4 text-white'>
       <Link to='/'>
@@ -8,15 +13,43 @@ export default function Header() {
       </Link>
 
       <nav className='flex items-center gap-6'>
-        <Link className='hover:underline hover:underline-offset-2' to='/about'>
-          About
-        </Link>
-        <Link
-          className='rounded-sm bg-white px-6 py-2 text-sky-500 shadow-md hover:bg-neutral-100'
-          to='/login'
-        >
-          Log In
-        </Link>
+        {auth?._id ? (
+          <>
+            <Link
+              className='hover:underline hover:underline-offset-2'
+              to='/patients'
+            >
+              Patients
+            </Link>
+            <Link
+              className='hover:underline hover:underline-offset-2'
+              to='/profile'
+            >
+              Profile
+            </Link>
+            <Link
+              className='rounded-sm bg-white px-6 py-2 text-sky-500 shadow-md hover:bg-neutral-100'
+              onClick={logout}
+            >
+              Log Out
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link
+              className='hover:underline hover:underline-offset-2'
+              to='/about'
+            >
+              About
+            </Link>
+            <Link
+              className='rounded-sm bg-white px-6 py-2 text-sky-500 shadow-md hover:bg-neutral-100'
+              to='/login'
+            >
+              Log In
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
