212220664d21571a287838c50d1ca047a8946348
Carlos <carlosaac232001@gmail.com>
2026-03-12 16:44:45 -0500

    chore: add separator between links

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -4,6 +4,7 @@ export default function AdminNav() {
   return (
     <nav className='flex justify-center gap-4 bg-sky-100 p-2 md:p-4'>
       <Link to='/admin/profile'>Profile</Link>
+      {'|'}
       <Link to='/admin/change-password'>Change Password</Link>
     </nav>
   );
